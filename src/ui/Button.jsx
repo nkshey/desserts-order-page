8ad07bf063f1fa583a2176dca58895dd61da6425
Nika Peikrishvili <peiqrishvili777@gmail.com>
2024-08-18 23:38:00 +0400

    adjusted button color

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,7 +5,7 @@ function Button({ children, type, onClick }) {
     round:
       "rounded-full border border-rose-50 p-0.5 transition-all duration-200 hover:bg-rose-50 group",
     addToCart:
-      "absolute -bottom-5 left-1/2 flex w-max -translate-x-1/2 transform items-center gap-2 rounded-full border border-rose-400 bg-rose-50 px-7 py-2.5 text-sm font-semibold text-rose-900 transition-all duration-200 hover:text-red hover:border-red",
+      "absolute -bottom-5 left-1/2 flex w-max -translate-x-1/2 transform items-center gap-2 rounded-full border border-rose-400 bg-[white] px-7 py-2.5 text-sm font-semibold text-rose-900 transition-all duration-200 hover:text-red hover:border-red",
   };
 
   if (onClick)
